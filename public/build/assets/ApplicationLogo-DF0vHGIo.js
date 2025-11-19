b050a8cc398f2303823254f9f4d6e34fd665c494
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-11-20 06:33:27 +0800

    refactor('build');

---
+++ public/build/assets/ApplicationLogo-DF0vHGIo.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-C6MKZYWX.js";const a="/build/assets/logo-BZmYTpjy.png";function t(o){return s.jsx("img",{src:a,alt:"App Logo",className:o.className||"h-20 w-auto"})}export{t as A};

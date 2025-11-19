4dc185c34f06be8fbd127106b2762f023a1594f2
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-11-20 06:43:41 +0800

    refactor('csrf');

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/sync/receive'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

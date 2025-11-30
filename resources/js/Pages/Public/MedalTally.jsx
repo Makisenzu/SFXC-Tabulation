5df572086b99f06baf64030a570d84e5ad5d909c
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-11-30 12:49:40 +0800

    style('test color 2');

--- resources/js/Pages/Public/MedalTally.jsx
+++ resources/js/Pages/Public/MedalTally.jsx
@@ -114,13 +114,13 @@ export default function MedalTally({ tallies: initialTallies }) {
     const getParticipantColor = (participantName) => {
         const name = participantName.toUpperCase();
         if (name.includes('CBE')) {
-            return 'border-l-4 border-yellow-500 bg-yellow-50';
+            return 'border-l-4 border-yellow-600 bg-yellow-50';
         } else if (name.includes('CTE')) {
-            return 'border-l-4 border-blue-500 bg-blue-50';
+            return 'border-l-4 border-blue-800 bg-blue-50';
         } else if (name.includes('CCJE')) {
-            return 'border-l-4 border-red-500 bg-red-50';
+            return 'border-l-4 border-red-600 bg-red-50';
         }
-        return 'border-l-4 border-gray-300';
+        return 'border-l-4 border-gray-300 bg-gray-50';
     };
 
     return (

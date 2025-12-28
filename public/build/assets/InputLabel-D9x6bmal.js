37a0f600c4817aa0acc0806a55681d75be5b435a
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-12-29 02:39:13 +0800

    chore('build dependencies');

---
+++ public/build/assets/InputLabel-D9x6bmal.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-CYmQ_d1T.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
